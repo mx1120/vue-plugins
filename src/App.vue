4740a8168358423e1ff972bc8a9a19d0918fd4c2
mx1120 <mengxiang@tbkt.cn>
2018-01-05 09:13:36 +0800

    mx

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,8 @@ export default {
 }
 </script>
 
-<style>
+<style rel="stylesheet/scss" lang="scss">
+    @import "assets/scss/reset";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
